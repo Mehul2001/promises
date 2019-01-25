9f69f20fa9987d00e79aa358177410da5b8ec8da
Mehul2001 <mmandviya@mail.csuchico.edu>
2019-01-24 22:04:15 -0800

    Implement callbackReview

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -19,8 +19,17 @@ var pluckFirstLineFromFile = function (filePath, callback) {
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url , function(error, response , body){
+     if(error) {
+       callback(error);
+     } else {
+       console.log(response.statusCode);
+       callback(null, response.statusCode);
+     }
+
+   });
+
 };
 
 // Export these functions so we can test them and reuse them in later exercises
